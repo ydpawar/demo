37232c51566c10fe4079eee5f661ffe44a34bb90
ydpawar <yogesh.pawar@indexnine.com>
2023-12-01 15:56:45 +0530

    changes for data and some of data of emp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import moment from "moment";
 
 function App() {
+  const employeeDetails = {
+    name: "John Doe",
+    age: 30,
+    position: "Software Engineer",
+  };
+
+  function getCurrentDate(separator = "") {
+    let newDate = new Date();
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    return `${year}${separator}${
+      month < 10 ? `0${month}` : `${month}`
+    }${separator}${date}`;
+  }
+
+  const data = [];
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h3>Form Details:</h3>
+      <p>
+        <h3>Name:</h3> {employeeDetails.name}
+      </p>
+      <p>
+        <h3>Email:</h3> {employeeDetails.age}
+      </p>
+      <p>
+        <h3>position:</h3> {employeeDetails.position}
+      </p>
+
+      <h3>Current Date: {moment(getCurrentDate()).format("YYYY/MM/DD")}</h3>
     </div>
   );
 }
